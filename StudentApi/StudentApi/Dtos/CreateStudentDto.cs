4c71a84ac5071382311d8cfe4c9538dbca697dfe
Albin Baby <albinbabayme05@gmail.com>
2026-03-27 09:14:43 +0100

    Validation added to Dto

--- StudentApi/StudentApi/Dtos/CreateStudentDto.cs
+++ StudentApi/StudentApi/Dtos/CreateStudentDto.cs
@@ -4,11 +4,19 @@ namespace StudentApi.Dtos
 {
     public class CreateStudentDto
     {
-        
+        [Required]
         public string? Name { get; set; }
+
+        [Range(1,100)]
         public int Age { get; set; }
+
+        [Required][EmailAddress]
         public string? Email { get; set; }
+
+        [Required][StringLength(60)]
         public string? Department { get; set; }
+
+        [StringLength(60)]
         public string? Course { get; set; }
         
     }
